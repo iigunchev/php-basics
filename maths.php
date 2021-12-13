a5dc84ca7a067ddc9da4dc405a90ffe3ce6737ed
iigunchev <iigunchev@gmail.com>
2021-12-13 18:11:08 +0100

    php-basics maths

---
+++ maths.php
@@ -0,0 +1,17 @@
+<?php
+// Absolute value
+    echo(abs(-6.7));  // returns 6.7
+    echo "<br>";
+// Rounded value to the next highest integer
+    echo(round(0.60));  // returns 1    
+    echo "<br>";
+// Highest value from a list of arguments
+    echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+    echo "<br>";
+// Lowest value from a list of arguments
+    echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+    echo "<br>";
+// Random number
+    echo(rand());
+    echo "<br>";
+?>
